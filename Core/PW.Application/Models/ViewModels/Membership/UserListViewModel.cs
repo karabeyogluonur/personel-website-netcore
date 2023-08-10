e6792af7d52d019cffbbdd1b024128be97425af4
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-10 13:03:10 +0300

    User listing added.

---
+++ Core/PW.Application/Models/ViewModels/Membership/UserListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW.Application.Models.ViewModels.Membership
+{
+    public class UserListViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName {
+            get
+            {
+                return String.Concat(FirstName, " ", LastName);
+            }
+        }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Presentation/PW.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PW.Application.Interfaces.Services.Membership;
+using PW.Application.Models.ViewModels.Membership;
+using PW.Domain.Entities.Membership;
+
+namespace PW.Web.Areas.Admin.Controllers
+{
+    public class UserController : BaseAdminController
+    {
+        IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return RedirectToAction("List", "User");
+        }
+
+        public async Task<IActionResult> List()
+        {
+            List<User> users = await _userService.GetAllAsync();
+
+            //TODO: Automapper will be added.
+
+            #region Automapper refactoring
+            List<UserListViewModel> userListViewModels = new List<UserListViewModel>();
+            foreach (var user in users)
+            {
+                UserListViewModel userListViewModel = new UserListViewModel();
+                userListViewModel.Id = user.Id;
+                userListViewModel.FirstName = user.FirstName;
+                userListViewModel.LastName = user.LastName;
+                userListViewModel.Email = user.Email;
+                userListViewModel.CreatedAt = user.CreatedAt;               
+                userListViewModels.Add(userListViewModel);
+            }
+            #endregion
+
+            return View(userListViewModels);
+        }
+    }
+}
