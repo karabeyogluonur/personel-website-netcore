cce60ce2a053a84e699e9f623edbff315028b405
karabeyogluonur <123563374+karabeyogluonur@users.noreply.github.com>
2023-08-10 23:00:29 +0300

    Password hash helper added.

---
+++ Core/PW.Application/Utilities/Helpers/PasswordHashHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW.Application.Utilities.Helpers
+{
+    public static class PasswordHashHelper
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool Verify(string password, string hashPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashPassword);
+        }
+    }
+}
